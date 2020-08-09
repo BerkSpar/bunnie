915351682a9b04445d2da64ba6c0ce4194f19edf
Felipe Passos <47111228+BerkSpar@users.noreply.github.com>
2020-08-08 23:27:52 -0300

    :fire: Remove Home

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -2,7 +2,6 @@ import 'package:rabbited/app/app_controller.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter/material.dart';
 import 'package:rabbited/app/app_widget.dart';
-import 'package:rabbited/app/modules/home/home_module.dart';
 
 class AppModule extends MainModule {
   @override
@@ -11,9 +10,7 @@ class AppModule extends MainModule {
       ];
 
   @override
-  List<Router> get routers => [
-        Router(Modular.initialRoute, module: HomeModule()),
-      ];
+  List<Router> get routers => [];
 
   @override
   Widget get bootstrap => AppWidget();
