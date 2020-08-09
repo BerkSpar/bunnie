f98fe7686a8c5a166732f957d7bb5053d9543618
Felipe Passos <47111228+BerkSpar@users.noreply.github.com>
2020-08-09 00:22:12 -0300

    :bug: Fix Int Cast

--- lib/app/modules/search/external/datasources/jikan_datasource.dart
+++ lib/app/modules/search/external/datasources/jikan_datasource.dart
@@ -11,7 +11,7 @@ class JikanDatasource implements SearchDatasource {
   @override
   Future<List<AnimeResultModel>> getSearch(String searchText,
       {int page}) async {
-    String path = 'http://api.jikan.moe/v3/search/anime?q=$searchText';
+    String path = 'http://api.jikan.moe/v3/search/anime?q=$searchText&limit=15';
     if (page != null) path += '&page=$page';
 
     final response = await dio.get(path);
