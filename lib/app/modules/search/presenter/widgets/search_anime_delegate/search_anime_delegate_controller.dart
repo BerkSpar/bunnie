fc57fac084f5f0ee4e734da91c79e985ef4fbf7e
Felipe Passos <47111228+BerkSpar@users.noreply.github.com>
2020-08-09 02:31:40 -0300

    :star: Add Search Anime Delegate

---
+++ lib/app/modules/search/presenter/widgets/search_anime_delegate/search_anime_delegate_controller.dart
@@ -0,0 +1,8 @@
+import 'package:mobx/mobx.dart';
+
+part 'search_anime_delegate_controller.g.dart';
+
+class SearchAnimeDelegateController = _SearchAnimeDelegateControllerBase
+    with _$SearchAnimeDelegateController;
+
+abstract class _SearchAnimeDelegateControllerBase with Store {}
