80e50e246bfcebfb90f2ae745c3af3528e727575
BerkSpar <47111228+BerkSpar@users.noreply.github.com>
2020-08-15 11:07:26 -0300

    Remove StatusBar Color

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:rabbited/app/app_module.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
-void main() => runApp(ModularApp(module: AppModule()));
+void main() {
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent,
+  ));
+  runApp(ModularApp(module: AppModule()));
+}
