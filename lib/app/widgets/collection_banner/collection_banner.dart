cd944da06ee7fef11560a3bc6643ec92e30cf6cd
Felipe Passos <berkspar@gmail.com>
2021-01-05 10:46:17 -0300

    add collection banner

--- lib/app/widgets/collection_banner/collection_banner.dart
+++ lib/app/widgets/collection_banner/collection_banner.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:rabbited/app/shared/models/collection.dart';
 import 'package:rabbited/app/widgets/rounded_image/rounded_image.dart';
@@ -15,11 +16,66 @@ class CollectionBanner extends StatelessWidget {
       padding: EdgeInsets.all(8),
       child: Stack(
         children: [
-          RoundedImage(
-            height: double.maxFinite,
-            width: double.maxFinite,
-            borderRadius: BorderRadius.circular(8),
-            imageUrl: collection.imageUrl,
+          Container(
+            foregroundDecoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(8),
+              gradient: LinearGradient(
+                begin: Alignment.bottomCenter,
+                end: Alignment.topCenter,
+                colors: [
+                  Colors.black.withOpacity(0.6),
+                  Colors.white.withOpacity(0.1),
+                ],
+              ),
+            ),
+            child: CachedNetworkImage(
+              imageUrl: collection.imageUrl,
+              height: double.maxFinite,
+              width: double.maxFinite,
+              imageBuilder: (_, imageProvider) {
+                return ClipRRect(
+                  borderRadius: BorderRadius.circular(8),
+                  child: Image(
+                    image: imageProvider,
+                    fit: BoxFit.cover,
+                  ),
+                );
+              },
+              progressIndicatorBuilder: (_, __, download) {
+                return Container(
+                  height: 80,
+                  width: 80,
+                  padding: EdgeInsets.all(16),
+                  child: CircularProgressIndicator(
+                    value: download.progress,
+                  ),
+                );
+              },
+              errorWidget: (_, __, ___) {
+                return Container(
+                  height: 80,
+                  width: 80,
+                  child: Icon(
+                    Icons.error,
+                    size: 40,
+                  ),
+                );
+              },
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomLeft,
+            child: Padding(
+              padding: EdgeInsets.all(16),
+              child: Text(
+                collection.name,
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+            ),
           ),
         ],
       ),
