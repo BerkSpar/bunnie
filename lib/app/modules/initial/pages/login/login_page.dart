a214442bb4646642137cf8da0990eb5f1479a298
Felipe Passos <berkspar@gmail.com>
2021-01-08 11:17:30 -0300

    add login animations

--- lib/app/modules/initial/pages/login/login_page.dart
+++ lib/app/modules/initial/pages/login/login_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:rabbited/app/utils/animations/fade_animation.dart';
 import 'package:rabbited/app/utils/bunnie_colors.dart';
 import 'login_controller.dart';
 
@@ -28,26 +29,32 @@ class _LoginPageState extends ModularState<LoginPage, LoginController> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             SizedBox(height: 64),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 32),
-              child: Text(
-                'Bunnie',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 32,
-                  fontWeight: FontWeight.bold,
+            FadeAnimation(
+              1,
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 32),
+                child: Text(
+                  'Bunnie',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 32,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
               ),
             ),
             SizedBox(height: 8),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 32),
-              child: Text(
-                'Welcome Back',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 22,
-                  fontWeight: FontWeight.w300,
+            FadeAnimation(
+              1.3,
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 32),
+                child: Text(
+                  'Welcome Back',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 22,
+                    fontWeight: FontWeight.w300,
+                  ),
                 ),
               ),
             ),
@@ -64,141 +71,162 @@ class _LoginPageState extends ModularState<LoginPage, LoginController> {
                 child: Column(
                   children: [
                     SizedBox(height: 32),
-                    TextFormField(
-                      decoration: InputDecoration(
-                        hintText: 'Username',
-                        filled: true,
-                        prefixIcon: Icon(Icons.person),
-                        border: OutlineInputBorder(),
+                    FadeAnimation(
+                      1.4,
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          hintText: 'Username',
+                          filled: true,
+                          prefixIcon: Icon(Icons.person),
+                          border: OutlineInputBorder(),
+                        ),
                       ),
                     ),
                     SizedBox(height: 8),
-                    TextFormField(
-                      decoration: InputDecoration(
-                        hintText: 'Password',
-                        filled: true,
-                        prefixIcon: Icon(Icons.lock),
-                        border: OutlineInputBorder(),
+                    FadeAnimation(
+                      1.5,
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          hintText: 'Password',
+                          filled: true,
+                          prefixIcon: Icon(Icons.lock),
+                          border: OutlineInputBorder(),
+                        ),
                       ),
                     ),
                     SizedBox(height: 16),
-                    GestureDetector(
-                      onTap: controller.recovery,
-                      child: Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 8),
-                        child: Text(
-                          'Forgot your password?',
-                          style: TextStyle(
-                            color: Colors.grey,
+                    FadeAnimation(
+                      1.6,
+                      child: GestureDetector(
+                        onTap: controller.recovery,
+                        child: Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 8),
+                          child: Text(
+                            'Forgot your password?',
+                            style: TextStyle(
+                              color: Colors.grey,
+                            ),
                           ),
                         ),
                       ),
                     ),
                     SizedBox(height: 32),
-                    Container(
-                      height: 48,
-                      width: double.maxFinite,
-                      child: RaisedButton(
-                        onPressed: controller.login,
-                        child: Text(
-                          'Login',
-                          style: TextStyle(
-                            fontSize: 16,
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
+                    FadeAnimation(
+                      1.7,
+                      child: Container(
+                        height: 48,
+                        width: double.maxFinite,
+                        child: RaisedButton(
+                          onPressed: controller.login,
+                          child: Text(
+                            'Login',
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          elevation: 0,
+                          color: BunnieColors.main.withGreen(130),
                         ),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(8),
-                        ),
-                        elevation: 0,
-                        color: BunnieColors.main.withGreen(130),
+                      ),
+                    ),
+                    SizedBox(height: 32),
+                    FadeAnimation(
+                      1.8,
+                      child: Row(
+                        children: [
+                          Expanded(
+                            child: Divider(
+                              endIndent: 16,
+                              indent: 8,
+                              thickness: 2,
+                            ),
+                          ),
+                          Text(
+                            'Othar ways to login',
+                            style: TextStyle(
+                              color: Colors.grey,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Expanded(
+                            child: Divider(
+                              endIndent: 8,
+                              indent: 16,
+                              thickness: 2,
+                            ),
+                          ),
+                        ],
                       ),
                     ),
                     SizedBox(height: 32),
                     Row(
                       children: [
                         Expanded(
-                          child: Divider(
-                            endIndent: 16,
-                            indent: 8,
-                            thickness: 2,
-                          ),
-                        ),
-                        Text(
-                          'Othar ways to login',
-                          style: TextStyle(
-                            color: Colors.grey,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        Expanded(
-                          child: Divider(
-                            endIndent: 8,
-                            indent: 16,
-                            thickness: 2,
-                          ),
-                        ),
-                      ],
-                    ),
-                    SizedBox(height: 32),
-                    Row(
-                      children: [
-                        Expanded(
-                          child: Container(
-                            height: 48,
-                            child: OutlineButton(
-                              onPressed: controller.loginWithGoogle,
-                              child: Row(
-                                mainAxisSize: MainAxisSize.min,
-                                children: [
-                                  FaIcon(
-                                    FontAwesomeIcons.google,
-                                    color: Colors.redAccent,
-                                  ),
-                                  SizedBox(width: 8),
-                                  Text(
-                                    'Google',
-                                    style: TextStyle(
-                                      fontSize: 16,
-                                      color: Colors.black,
-                                      fontWeight: FontWeight.bold,
+                          child: FadeAnimation(
+                            1.8,
+                            child: Container(
+                              height: 48,
+                              child: OutlineButton(
+                                onPressed: controller.loginWithGoogle,
+                                child: Row(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    FaIcon(
+                                      FontAwesomeIcons.google,
+                                      color: Colors.redAccent,
                                     ),
-                                  ),
-                                ],
-                              ),
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(8),
+                                    SizedBox(width: 8),
+                                    Text(
+                                      'Google',
+                                      style: TextStyle(
+                                        fontSize: 16,
+                                        color: Colors.black,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(8),
+                                ),
                               ),
                             ),
                           ),
                         ),
                         SizedBox(width: 16),
                         Expanded(
-                          child: Container(
-                            height: 48,
-                            child: OutlineButton(
-                              onPressed: controller.loginWithFacebook,
-                              child: Row(
-                                mainAxisSize: MainAxisSize.min,
-                                children: [
-                                  FaIcon(
-                                    FontAwesomeIcons.facebook,
-                                    color: Colors.blue,
-                                  ),
-                                  SizedBox(width: 8),
-                                  Text(
-                                    'Facebook',
-                                    style: TextStyle(
-                                      fontSize: 16,
-                                      color: Colors.black,
-                                      fontWeight: FontWeight.bold,
+                          child: FadeAnimation(
+                            1.8,
+                            child: Container(
+                              height: 48,
+                              child: OutlineButton(
+                                onPressed: controller.loginWithFacebook,
+                                child: Row(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    FaIcon(
+                                      FontAwesomeIcons.facebook,
+                                      color: Colors.blue,
                                     ),
-                                  ),
-                                ],
-                              ),
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(8),
+                                    SizedBox(width: 8),
+                                    Text(
+                                      'Facebook',
+                                      style: TextStyle(
+                                        fontSize: 16,
+                                        color: Colors.black,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(8),
+                                ),
                               ),
                             ),
                           ),
